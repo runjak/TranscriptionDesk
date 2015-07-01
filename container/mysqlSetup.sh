f931632f7a5fb88ba0340611ac35eb7a8555a740
Jakob Runge <sicarius@g4t3.de>
2015-07-01 17:41:21 +0200

    Provisioning for Vagrant appears to work
    - Testing with Olli now
    - Need to fix Docker (also with vagrant, possibly)

--- container/mysqlSetup.sh
+++ container/mysqlSetup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-#This script will be executed inside the docker container.
+#This script will be executed inside a container.
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 1234'
+apt-get install -y mysql-server
 service mysql start
 echo "Creating database: TranscriptionDesk"
-echo "CREATE DATABASE TranscriptionDesk CHARACTER SET utf8;"|mysql -uroot -p
+echo "CREATE DATABASE TranscriptionDesk CHARACTER SET utf8;"|mysql -uroot -p1234
 echo "Creating tables from create.sql"
-mysql -uroot -p TranscriptionDesk < $1
+mysql -uroot -p1234 TranscriptionDesk < $1
