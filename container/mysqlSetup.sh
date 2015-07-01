8f913d7ef6c7697bcc484dc3a1e77389ac528ad4
Jakob Runge <sicarius@g4t3.de>
2015-07-01 17:26:46 +0200

    Improving provisioning…

--- container/mysqlSetup.sh
+++ container/mysqlSetup.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #This script will be executed inside the docker container.
 service mysql start
-echo "Setting MySql password: 1234"
-echo "SET PASSWORD FOR 'root'@'localhost' = PASSWORD('1234');"|mysql -uroot
 echo "Creating database: TranscriptionDesk"
-echo "CREATE DATABASE TranscriptionDesk CHARACTER SET utf8;"|mysql -uroot -p1234
+echo "CREATE DATABASE TranscriptionDesk CHARACTER SET utf8;"|mysql -uroot -p
 echo "Creating tables from create.sql"
-mysql -uroot -p1234 TranscriptionDesk < create.sql
+mysql -uroot -p TranscriptionDesk < $1
