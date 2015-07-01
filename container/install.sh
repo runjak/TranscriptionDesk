d0569158e34931f14e4e35b17797e3e0908435cb
Jakob Runge <sicarius@g4t3.de>
2015-07-01 20:56:31 +0200

    Got the Vagrant setup working
    - Now we need to see how it works on Ollis machine.
    - The Makefile can now be used to setup both vagrant and docker.
    - I've abandoned work to use docker as a provider for vagrant,
      as this setup appears to be working ok now.
    - I still need to fix create.sql to use good keys, and adjust the ER diagram.

--- container/install.sh
+++ container/install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-#This script will be executed inside a container.
+# This script will be executed inside a container.
+# $1 is expected to be the create.sql file.
 apt-get update
+ap-get dist-upgrade
+apt-get install -y apache2-mpm-prefork libapache2-mod-php5 php5 php5-mysql
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 1234'
 apt-get install -y mysql-server
@@ -9,3 +12,4 @@ echo "Creating database: TranscriptionDesk"
 echo "CREATE DATABASE TranscriptionDesk CHARACTER SET utf8;"|mysql -uroot -p1234
 echo "Creating tables from create.sql"
 mysql -uroot -p1234 TranscriptionDesk < $1
+service mysql stop
