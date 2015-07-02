0cc1f6b7a3977b53cab05f5910d2c6529187ad6c
Jakob Runge <sicarius@g4t3.de>
2015-07-02 11:41:02 +0200

    Corrected index.php for Robert
    - 'correct' version was lost in merge

--- src/index.php
+++ src/index.php
@@ -1,29 +1,38 @@
-<!DOCTYPE HTML>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>Some testpage</title>
-  <meta name="description" content="This does… a thing.">
-  <meta name="author" content="https://github.com/runjak/TranscriptionDesk">
-  </head>
-  <body>
-    <h1>Yeah, it worked!</h1>
-    <p>Have a look at some <a href="omekaThumbnails.php">thumbnails</a>!</p>
-    <p>It appears that you've got this part working :)</p>
-    <p>Another <a href="phptestfile.php">test</a>.</p>
-    <?php
-      phpinfo();
-    ?>
-    <h1>Testing database:</h1>
-    <?php
-      require_once 'config.php';
-      $db = Config::getDB();
-      $set = $db->query("SHOW TABLES");
-      while($r = $set->fetch_assoc()){
-        foreach($r as $k => $v){
-            echo "$k => $v<br>";
-        }
-      }
-    ?>
-  </body>
+    <head>
+        <meta charset="utf-8">
+        <title>Main page</title>
+        <meta name="descirption" content="Main Page">
+        <meta name="author" content="https://github.com/runjak/TranscriptionDesk">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Bootstrap -->
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- Navigation -->
+        <?php
+            include_once('header.php');
+        ?>
+        <!-- Page Content -->
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1>Welcome to Trancriptiondesk!</h1>
+                    <p class="lead">A citizen science project to transcribe historical texts is about to be created!</p>
+                    <p>Find out more in our about page. Or create an <a href="register.php">account</a>.</p>
+                    <ul class="list-unstyled">
+                        <li>Bootstrap v3.3.1</li>
+                        <li>jQuery v1.11.1</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
+
