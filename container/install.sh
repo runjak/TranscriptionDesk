e1ab4ac9810501e93f248ba541eef5bb7027ead0
Jakob Runge <sicarius@g4t3.de>
2015-07-02 14:34:46 +0200

    Some progress with user authentication
    - Added opauth together with GitHub strategy.
    - Updated container construction to take into account mod_rewrite.
    - Complete authentication with GitHub doesn't work yet.
    - See #6 for more details.

--- container/install.sh
+++ container/install.sh
@@ -4,6 +4,7 @@
 apt-get update
 apt-get dist-upgrade
 apt-get install -y apache2-mpm-prefork libapache2-mod-php5 php5 php5-mysql
+# Setting up MySQL:
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password 1234'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password 1234'
 apt-get install -y mysql-server
@@ -13,3 +14,6 @@ echo "CREATE DATABASE TranscriptionDesk CHARACTER SET utf8;"|mysql -uroot -p1234
 echo "Creating tables from create.sql"
 mysql -uroot -p1234 TranscriptionDesk < $1
 service mysql stop
+# Configuring apache2 for mod_rewrite and AllowOverride:
+a2enmod rewrite
+echo -e "<Directory \"/var/www/html\">\nAllowOverride All\n</Directory>" >> /etc/apache2/sites-enabled/000-default.conf
