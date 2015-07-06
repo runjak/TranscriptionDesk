6c9e940d096d14b62f7b2151552eca98b78c9ce2
Jakob Runge <sicarius@g4t3.de>
2015-07-06 16:41:18 +0200

    Implemented UserManager.{verify,login,logout}.
    - It should now be possible to have working user sessions.
    - UserManager.login appears to work, and I'll adjust the navbar for the other cases.

--- src/userManager.php
+++ src/userManager.php
@@ -25,23 +25,65 @@ class UserManager {
         return $this->opauth;
     }
     /**
+        @param $user User
+        @return $secret String
+        We can compute a users session secret as a hash of the following data:
+        - userId
+        - authenticationMethod
+        - lastLogin
+        - Opauth security_salt
+        Since the session secret changes regularly
+        it will be sufficient to use a regular cryptographic hash function
+        like SHA* instead of scrypt, bcrypt or similarly sophisticated algorithms.
+        See [1] for documentation of hash().
+        [1]: https://secure.php.net/manual/en/function.hash.php
+    */
+    private function sessionSecret($user){
+        $data  = $user->getUserId();
+        $data .= $user->getAuthenticationMethod();
+        $data .= $user->getLastLogin();
+        $data .= $this->config['security_salt'];
+        return hash('sha512', $data, false);
+    }
+    /**
         @return $user User || null
         Verifies the current session.
         A User is returned iff the session is valid.
     */
     public function verify(){
-        //FIXME IMPLEMENT
+        session_start();
+        if(!array_key_exists('userId', $_SESSION)
+         ||!array_key_exists('secret', $_SESSION)){
+            return null;
+        }
+        $userId = $_SESSION['userId'];
+        $secret = $_SESSION['secret'];
+        $user = User::fromUserId($userId);
+        if($user === null){ return null; }
+        if($secret === $this->sessionSecret($user)){
+            return $user;
+        }
+        return null;
     }
     /**
         @param $user User
         Marks the user as logged in.
     */
     public function login($user){
-        //FIXME IMPLEMENT
+        /*
+          First thing to do is update the users timestamp.
+          This is necessary to integrate it into the session secret.
+        */
+        $user->updateLastLogin();
+        $secret = $this->sessionSecret($user);
+        session_start();
+        $_SESSION['userId'] = $user->getUserId();
+        $_SESSION['secret'] = $secret;
     }
     /**
         Marks the current user as logged out.
     */
     public function logout(){
+        session_destroy();
     }
 }
