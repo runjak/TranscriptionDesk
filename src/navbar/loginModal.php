ba54512f53cde48210c2a91f60241409f443fd4a
Jakob Runge <sicarius@g4t3.de>
2015-07-06 12:53:50 +0200

    Added entries to navbar/loginModal.php
    - GitHub, Twitter, Facebook
    - Need to add strategies for Facebook, Twitter
    - Need to vertical align and make bold text in loginModal.php.

--- src/navbar/loginModal.php
+++ src/navbar/loginModal.php
@@ -1,42 +1,36 @@
 <div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="loginModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                 <h4 class="modal-title" id="loginModalLabel">Log in or register</h4>
-            </div> <!-- /.modal-header -->
+            </div>
             <div class="modal-body">
-                <form role="form">
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="text" class="form-control" id="uLogin" placeholder="Login">
-                            <label for="uLogin" class="input-group-addon glyphicon glyphicon-user"></label>
-                        </div>
-                    </div> <!-- /.form-group -->
-                    <div class="form-group">
-                        <div class="input-group">
-                            <input type="password" class="form-control" id="uPassword" placeholder="Password">
-                            <label for="uPassword" class="input-group-addon glyphicon glyphicon-lock"></label>
-                        </div> <!-- /.input-group -->
-                    </div> <!-- /.form-group -->
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"> Remember me
-                        </label>
-                    </div> <!-- /.checkbox -->
-                </form>
-
-            </div> <!-- /.modal-body -->
-
-            <div class="modal-footer">
-                <button class="form-control btn btn-primary">Ok</button>
-                <div class="progress">
-                    <div class="progress-bar progress-bar-primary" role="progressbar" aria-valuenow="1" aria-valuemin="1" aria-valuemax="100" style="width: 0%;">
-                        <span class="sr-only">progress</span>
-                    </div>
-                </div>
-            </div> <!-- /.modal-footer -->
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+                <h3>Authenticate via…</h3><?php
+                    foreach(array(
+                        array(
+                            'auth' => 'facebook',
+                            'text' => 'facebook',
+                            'img'  => 'FBLogo.png'
+                        ),
+                        array(
+                            'auth' => 'github',
+                            'text' => 'GitHub',
+                            'img'  => 'GitHubLogo.png'
+                        ),
+                        array(
+                            'auth' => 'twitter',
+                            'text' => 'Twitter',
+                            'img'  => 'TwitterLogo.png'
+                        )
+                    ) as $entry){
+                        $auth = $entry['auth'];
+                        $text = $entry['text'];
+                        $img = $entry['img'];?>
+                <a class="btn btn-default loginStrategy" href="auth/<?php echo $auth;?>">
+                    <img src="img/<?php echo $img;?>" class="pull-left"><?php echo $text;?>
+                </a><?php } ?>
+            </div>
+        </div>
+    </div>
+</div>
