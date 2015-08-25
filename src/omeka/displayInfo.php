d4e2b442a911a7b4907b6a27a01b51a89a4668c0
Jakob Runge <sicarius@g4t3.de>
2015-08-25 14:19:30 +0200

    Added method OmekaDisplayInfo.shouldDisplay()
    - Evaluates to true, iff:
      - An OmekaDisplayInfo is public and featured.
      - An OmekaDisplayInfo is featured and a user is logged in.
    - These semantics where discussed in notes/2015-06-19.md

--- src/omeka/displayInfo.php
+++ src/omeka/displayInfo.php
@@ -20,4 +20,20 @@ class OmekaDisplayInfo extends OmekaTimestamped {
     public function isFeatured(){
         return $this->data['featured'];
     }
+    /**
+        @return should Boolean
+        In [1] we noted, that things that can be public and/or featured should be displayed under the following conditions:
+        * The thing is public and featured.
+        * The thing is featured and a user is logged in.
+        [1]: https://github.com/runjak/TranscriptionDesk/blob/master/notes/2015-06-19.md
+    */
+    public function shouldDisplay(){
+        //¬featured -> ¬shouldDisplay():
+        if(!$this->isFeatured()) return false;
+        //public && featured -> shouldDisplay():
+        if($this->isPublic()) return true;
+        //Depends on login:
+        $user = Config::getUserManager()->verify();
+        return ($user !== null);
+    }
 }
