b12aee740d987d9e16f7ab170f18a0afcab7b02b
Jakob Runge <sicarius@g4t3.de>
2015-09-17 14:20:44 +0200

    Adjusted mufiForm.js to use require.js
    - Also added mufiFormTest.php to demo the current usage.

---
+++ src/mufiFormTest.php
@@ -0,0 +1,22 @@
+<html lang="en">
+    <head>
+        <?php require_once 'head.php';?>
+        <title>Main page</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h1>Test page for mufiForm.js</h1>
+                    <div id="a"></div>
+                    <div id="b"></div>
+                </div>
+            </div>
+        </div>
+        <script>
+            require(['mufiForm'], function(mufiForm){
+                mufiForm.showMufiInput('#a','#b');
+            });
+        </script>
+    </body>
+</html>
